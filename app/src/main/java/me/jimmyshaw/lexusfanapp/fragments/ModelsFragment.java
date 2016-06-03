2cc8361bdf590862ab71e247e7acc7ec6f20cdf5
Jim Shaw <jimxshaw@gmail.com>
2016-06-03 13:16:49 -0500

    added RecyclerView Adapter and ViewHolder inner classes to ModelsFragment

--- app/src/main/java/me/jimmyshaw/lexusfanapp/fragments/ModelsFragment.java
+++ app/src/main/java/me/jimmyshaw/lexusfanapp/fragments/ModelsFragment.java
@@ -2,6 +2,7 @@ package me.jimmyshaw.lexusfanapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,11 @@ public class ModelsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        ContentAdapter adapter = new ContentAdapter();
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_models, container, false);
-
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return recyclerView;
     }
@@ -25,6 +29,27 @@ public class ModelsFragment extends Fragment {
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.fragment_models_card_view_item, parent, false));
 
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set number of Cards in the recycler view.
+        private static final int LENGTH = 18;
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
         }
     }
 
