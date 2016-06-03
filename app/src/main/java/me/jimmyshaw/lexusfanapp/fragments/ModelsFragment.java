96f527f1811dd52d2cb14fe1adb0b642998914a5
Jim Shaw <jimxshaw@gmail.com>
2016-06-03 12:59:38 -0500

    implemented card view item layout

--- app/src/main/java/me/jimmyshaw/lexusfanapp/fragments/ModelsFragment.java
+++ app/src/main/java/me/jimmyshaw/lexusfanapp/fragments/ModelsFragment.java
@@ -2,6 +2,7 @@ package me.jimmyshaw.lexusfanapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,19 @@ public class ModelsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_models, container, false);
 
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_models, container, false);
+
+
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.fragment_models_card_view_item, parent, false));
+
+        }
     }
 
 }
