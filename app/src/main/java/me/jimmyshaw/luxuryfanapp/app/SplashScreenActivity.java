24c95f4a1af1dd060b4d7c9d78aee6cacee3e121
Jim Shaw <jimxshaw@gmail.com>
2016-06-09 15:56:30 -0500

    added a delay to splash screen. modified android manifest to prevent back button from going to splash screen

--- app/src/main/java/me/jimmyshaw/luxuryfanapp/app/SplashScreenActivity.java
+++ app/src/main/java/me/jimmyshaw/luxuryfanapp/app/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package me.jimmyshaw.luxuryfanapp.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -10,8 +11,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, ModelActivity.class);
-        startActivity(intent);
-        finish();
+        // Duration of splash screen in milliseconds.
+        final int DISPLAY_LENGTH = 2000;
+
+        // The handler will start a new activity and then close the splash screen after
+        // a set duration.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, ModelActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DISPLAY_LENGTH);
+
     }
 }
