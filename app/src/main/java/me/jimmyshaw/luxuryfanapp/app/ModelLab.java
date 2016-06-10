f382718d92045ead21b5a98d62e4c8e6a531e183
Jim Shaw <jimxshaw@gmail.com>
2016-06-10 13:11:10 -0500

    implemented ModelLab singleton adding to it all of ModelFragment's static data retrieval methods.

--- app/src/main/java/me/jimmyshaw/luxuryfanapp/app/ModelLab.java
+++ app/src/main/java/me/jimmyshaw/luxuryfanapp/app/ModelLab.java
@@ -1,6 +1,11 @@
 package me.jimmyshaw.luxuryfanapp.app;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import me.jimmyshaw.luxuryfanapp.R;
+
 /*
     ModelLab is a Bill Pugh singleton pattern implementation. Before Java 5, certain singleton
     implementations failed in some situations where too many threads try to get the singleton class
@@ -11,12 +16,112 @@ package me.jimmyshaw.luxuryfanapp.app;
 */
 public class ModelLab {
 
+    private final String API_KEY = "k5whpdvu4rf2h2gj3wuzaysg";
+
     private ModelLab() {
 
     }
 
     public static ModelLab getInstance() {
         return ModelLabHelper.INSTANCE;
+    }
+
+    public String getApiKey() {
+        return API_KEY;
+    }
+
+    public Map<String, String> getModelsOptions(String category) {
+        // Create the hash map of GET request query parameters.
+        Map<String, String> options = new HashMap<>();
+        options.put("state", "new");
+        options.put("year", "2016");
+        options.put("view", "basic");
+        options.put("api_key", API_KEY);
+        // A null category means there's no filter and the user will see all car models. So we
+        // don't have to put the category in the HashMap.
+        if (category != null) {
+            options.put("category", category);
+        }
+        return options;
+    }
+
+    public Map<String, String> getModelsAndPricesMap() {
+        Map<String, String> modelsAndPrices = new HashMap<>();
+        modelsAndPrices.put("CT 200h", "$31,250");
+        modelsAndPrices.put("ES 300h", "$41,020");
+        modelsAndPrices.put("ES 350", "$38,100");
+        modelsAndPrices.put("GS 200t", "$45,615");
+        modelsAndPrices.put("GS 350", "$50,000");
+        modelsAndPrices.put("GS 450h", "$63,080");
+        modelsAndPrices.put("GS F", "$84,440");
+        modelsAndPrices.put("GX 460", "$50,780");
+        modelsAndPrices.put("IS 200t", "$37,325");
+        modelsAndPrices.put("IS 300", "$39,700");
+        modelsAndPrices.put("IS 350", "$40,870");
+        modelsAndPrices.put("LS 460", "$72,520");
+        modelsAndPrices.put("LS 600h L", "$120,440");
+        modelsAndPrices.put("LX 570", "$88,880");
+        modelsAndPrices.put("NX 200t", "$34,965");
+        modelsAndPrices.put("NX 300h", "$39,720");
+        modelsAndPrices.put("RC 200t", "$39,995");
+        modelsAndPrices.put("RC 300", "$42,610");
+        modelsAndPrices.put("RC 350", "$42,780");
+        modelsAndPrices.put("RC F", "$62,805");
+        modelsAndPrices.put("RX 350", "$41,900");
+        modelsAndPrices.put("RX 450h", "$52,235");
+
+        return modelsAndPrices;
+    }
+
+    public int bindImage(String modelName) {
+        switch (modelName) {
+            case "CT 200h":
+                return R.drawable.model_ct_200h;
+            case "ES 300h":
+                return R.drawable.model_es_300h;
+            case "ES 350":
+                return R.drawable.model_es_350;
+            case "GS 200t":
+                return R.drawable.model_gs_200t;
+            case "GS 350":
+                return R.drawable.model_gs_350;
+            case "GS 450h":
+                return R.drawable.model_gs_450h;
+            case "GS F":
+                return R.drawable.model_gs_f;
+            case "GX 460":
+                return R.drawable.model_gx_460;
+            case "IS 200t":
+                return R.drawable.model_is_200t;
+            case "IS 300":
+                return R.drawable.model_is_300;
+            case "IS 350":
+                return R.drawable.model_is_350;
+            case "LS 460":
+                return R.drawable.model_ls_460;
+            case "LS 600h L":
+                return R.drawable.model_ls_600h_l;
+            case "LX 570":
+                return R.drawable.model_lx_570;
+            case "NX 200t":
+                return R.drawable.model_nx_200t;
+            case "NX 300h":
+                return R.drawable.model_nx_300h;
+            case "RC 200t":
+                return R.drawable.model_rc_200t;
+            case "RC 300":
+                return R.drawable.model_rc_300;
+            case "RC 350":
+                return R.drawable.model_rc_350;
+            case "RC F":
+                return R.drawable.model_rc_f;
+            case "RX 350":
+                return R.drawable.model_rx_350;
+            case "RX 450h":
+                return R.drawable.model_rx_450h;
+            default:
+                return R.drawable.model_logo;
+        }
     }
 
     public static class ModelLabHelper {
--- app/src/main/java/me/jimmyshaw/luxuryfanapp/fragments/ModelFragment.java
+++ app/src/main/java/me/jimmyshaw/luxuryfanapp/fragments/ModelFragment.java
@@ -21,11 +21,11 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import me.jimmyshaw.luxuryfanapp.R;
+import me.jimmyshaw.luxuryfanapp.app.ModelLab;
 import me.jimmyshaw.luxuryfanapp.edmunds.Model;
 import me.jimmyshaw.luxuryfanapp.edmunds.Models;
 import me.jimmyshaw.luxuryfanapp.edmunds.util.EdmundsService;
@@ -36,9 +36,10 @@ import retrofit2.Response;
 
 public class ModelFragment extends Fragment {
 
-    private final String API_KEY = "k5whpdvu4rf2h2gj3wuzaysg";
-
     private static final String ARG_CATEGORY = "model_category";
+
+    private ModelLab modelLab;
+
     private String mCategory;
 
     private RecyclerView mModelRecyclerView;
@@ -63,12 +64,16 @@ public class ModelFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Off load many of our static data retrieval methods to singleton class. 
+        modelLab = ModelLab.getInstance();
+
         // Retrieve the category argument that's been set through the newInstance creation of
         // this fragment and assign it to a variable.
         mCategory = getArguments().getString(ARG_CATEGORY);
 
         // Instantiate our map of known models and prices to be used later in the ViewHolder.
-        mModelsAndPrices = getModelsAndPricesMap();
+        mModelsAndPrices = modelLab.getModelsAndPricesMap();
 
         // Add a progress dialog spinner that will display during interactions with the server.
         final ProgressDialog progressDialog = new ProgressDialog(getActivity(), R.style.ProgressDialogTheme);
@@ -80,7 +85,7 @@ public class ModelFragment extends Fragment {
         EdmundsService service = EdmundsServiceGenerator.createService(EdmundsService.class);
 
         // Use a map of options to actually query the backend API server with a GET request.
-        Call<Models> call = service.getModels(getModelsOptions());
+        Call<Models> call = service.getModels(modelLab.getModelsOptions(mCategory));
         call.enqueue(new Callback<Models>() {
             @Override
             public void onResponse(Call<Models> call, Response<Models> response) {
@@ -109,49 +114,6 @@ public class ModelFragment extends Fragment {
         updateUI();
 
         return mModelRecyclerView;
-    }
-
-    private Map<String, String> getModelsOptions() {
-        // Create the hash map of GET request query parameters.
-        Map<String, String> options = new HashMap<>();
-        options.put("state", "new");
-        options.put("year", "2016");
-        options.put("view", "basic");
-        options.put("api_key", API_KEY);
-        // A null category means there's no filter and the user will see all car models. So we
-        // don't have to put the category in the HashMap.
-        if (mCategory != null) {
-            options.put("category", mCategory);
-        }
-        return options;
-    }
-
-    private Map<String, String> getModelsAndPricesMap() {
-        Map<String, String> modelsAndPrices = new HashMap<>();
-        modelsAndPrices.put("CT 200h", "$31,250");
-        modelsAndPrices.put("ES 300h", "$41,020");
-        modelsAndPrices.put("ES 350", "$38,100");
-        modelsAndPrices.put("GS 200t", "$45,615");
-        modelsAndPrices.put("GS 350", "$50,000");
-        modelsAndPrices.put("GS 450h", "$63,080");
-        modelsAndPrices.put("GS F", "$84,440");
-        modelsAndPrices.put("GX 460", "$50,780");
-        modelsAndPrices.put("IS 200t", "$37,325");
-        modelsAndPrices.put("IS 300", "$39,700");
-        modelsAndPrices.put("IS 350", "$40,870");
-        modelsAndPrices.put("LS 460", "$72,520");
-        modelsAndPrices.put("LS 600h L", "$120,440");
-        modelsAndPrices.put("LX 570", "$88,880");
-        modelsAndPrices.put("NX 200t", "$34,965");
-        modelsAndPrices.put("NX 300h", "$39,720");
-        modelsAndPrices.put("RC 200t", "$39,995");
-        modelsAndPrices.put("RC 300", "$42,610");
-        modelsAndPrices.put("RC 350", "$42,780");
-        modelsAndPrices.put("RC F", "$62,805");
-        modelsAndPrices.put("RX 350", "$41,900");
-        modelsAndPrices.put("RX 450h", "$52,235");
-
-        return modelsAndPrices;
     }
 
     private void updateUI() {
@@ -186,56 +148,6 @@ public class ModelFragment extends Fragment {
         return bitmap;
     }
 
-    private int bindImage(String modelName) {
-        switch (modelName) {
-            case "CT 200h":
-                return R.drawable.model_ct_200h;
-            case "ES 300h":
-                return R.drawable.model_es_300h;
-            case "ES 350":
-                return R.drawable.model_es_350;
-            case "GS 200t":
-                return R.drawable.model_gs_200t;
-            case "GS 350":
-                return R.drawable.model_gs_350;
-            case "GS 450h":
-                return R.drawable.model_gs_450h;
-            case "GS F":
-                return R.drawable.model_gs_f;
-            case "GX 460":
-                return R.drawable.model_gx_460;
-            case "IS 200t":
-                return R.drawable.model_is_200t;
-            case "IS 300":
-                return R.drawable.model_is_300;
-            case "IS 350":
-                return R.drawable.model_is_350;
-            case "LS 460":
-                return R.drawable.model_ls_460;
-            case "LS 600h L":
-                return R.drawable.model_ls_600h_l;
-            case "LX 570":
-                return R.drawable.model_lx_570;
-            case "NX 200t":
-                return R.drawable.model_nx_200t;
-            case "NX 300h":
-                return R.drawable.model_nx_300h;
-            case "RC 200t":
-                return R.drawable.model_rc_200t;
-            case "RC 300":
-                return R.drawable.model_rc_300;
-            case "RC 350":
-                return R.drawable.model_rc_350;
-            case "RC F":
-                return R.drawable.model_rc_f;
-            case "RX 350":
-                return R.drawable.model_rx_350;
-            case "RX 450h":
-                return R.drawable.model_rx_450h;
-            default:
-                return R.drawable.model_logo;
-        }
-    }
 
     private void showInfoDialog(int titleId, int bodyId) {
         String title = getResources().getString(titleId);
@@ -289,7 +201,7 @@ public class ModelFragment extends Fragment {
 
             // Use Picasso to resize the model image and load it into our image view.
             Picasso.with(getActivity())
-                    .load(bindImage(mModel.getName()))
+                    .load(modelLab.bindImage(mModel.getName()))
                     .placeholder(R.drawable.model_logo)
                     .into(mImage);
 
